29898d4fc10d72caf139a4a92599d6e405f1dc96
cryphon <yvanroes@gmail.com>
2024-09-02 06:51:30 +0200

    added code comments to the single-thread code example

--- lecture_1/src/single_thread.cpp
+++ lecture_1/src/single_thread.cpp
@@ -3,7 +3,10 @@
 
 
 int main(void) {
+    // getpid is a built-in method to get the active process id
     int process_id = getpid();
+
+    // this loop is used to observe the IDs of processes overtime to see how they behave
     for (size_t i = 0; i < 5; i++) {
         printf("process #%d: waiting ... %d\n", process_id, i);
         sleep(1);
