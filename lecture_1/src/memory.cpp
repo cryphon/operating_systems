d0ec4f8e930a7cadf0ed09a708388738ea583b8f
cryphon <yvanroes@gmail.com>
2024-09-02 06:59:29 +0200

    added code comments on the memory allocation example

--- lecture_1/src/memory.cpp
+++ lecture_1/src/memory.cpp
@@ -5,12 +5,17 @@
 
 
 int main(void) {
+    // data is equal to 4 bytes as the size of an int is 4 bytes
     int *data = (int*)malloc(1 * sizeof(int));
     *data = getpid();
 
+    // here, array is 10 * 4 bytes.
     int *array = (int*)malloc(10 * sizeof(int));
 
-    printf("array[0] - %d\n", 0[array]);
+    // array[i] is defined as *(array + i) thus i[array] is also valid code
+    printf("array[0] - %d\n", array[0]);
+    printf("0[array] - %d\n", 0[array]);
+
     // print mem addr and val
     printf("dta addr: = %p, value (pid) = %d\n", data, *data);
 
